4f6c54fbdd78162f57a921cb6629053a6ae50ef6
firozbs23 <firoz@brainstation-23.com>
2021-12-09 12:43:25 +0600

    BooksModel updated

---
+++ core/src/main/java/com/aem/demo/core/models/Book.java
@@ -0,0 +1,22 @@
+package com.aem.demo.core.models;
+
+public class Book {
+    private String book;
+    private String subject;
+
+    public String getBook() {
+        return book;
+    }
+
+    public void setBook(String book) {
+        this.book = book;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+}
---
+++ core/src/main/java/com/aem/demo/core/models/BooksModel.java
@@ -0,0 +1,48 @@
+package com.aem.demo.core.models;
+
+import org.apache.sling.api.SlingHttpServletRequest;
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.models.annotations.Model;
+
+import javax.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.List;
+
+@Model(adaptables = {Resource.class, SlingHttpServletRequest.class})
+public class BooksModel {
+
+    private String test;
+    private List<Book> books;
+
+    @PostConstruct
+    protected void init() {
+        setTest("I am from Test model");
+        setBooks();
+    }
+
+    public String getTest() {
+        return test;
+    }
+
+    private void setTest(String test) {
+        this.test = test;
+    }
+
+    public List<Book> getBooks() {
+        return books;
+    }
+
+    private void setBooks() {
+        books = new ArrayList<>();
+
+        Book book = new Book();
+        book.setBook("Book 1");
+        book.setSubject("Subject 1");
+        books.add(book);
+
+        Book book2 = new Book();
+        book2.setBook("Book 2");
+        book2.setSubject("Subject 2");
+        books.add(book2);
+    }
+}
